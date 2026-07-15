9a43a92ab7819a97717605a3513f06615378b75b
Pavlo-Myshev <kop8100@gmail.com>
2026-07-15 23:59:45 +0200

    PM Assistant v2.1 Final

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='pm-assistant-v2';
+const CACHE='pm-assistant-v21';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/pm-logo.jpeg','./assets/pavel-founder.jpeg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
